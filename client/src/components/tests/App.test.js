ca42d4c57b185360056c551eeb047aca6deaac51
Kevin Lonergan <kevinlonergan@Kevins-MacBook-Pro-2.local>
2020-01-28 23:10:42 -0800

    setup test for real this time

---
+++ client/src/components/tests/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+
+import App from '../App.jsx';
+
+describe('should be true', () => {
+
+  it('True is true', () => {
+
+    expect(true).toBe(true);
+  })
+
+});
